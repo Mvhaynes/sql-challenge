7b17f3e4a370c65766f22040bb298acbfb22effc
melissa <melissahaynes@Melissas-Air-2.lan>
2021-03-05 18:40:33 -0600

    queries

--- queries.sql
+++ queries.sql
@@ -14,4 +14,14 @@ from dept_manager as man
 join employees as emp on emp.emp_no = man.emp_no
 join departments on departments.dept_no = man.dept_no;
 
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+select dept_emp.emp_no, employees.first_name, employees.last_name, departments.dept_name
+from dept_emp
+left join employees on dept_emp.emp_no = employees.emp_no
+left join departments on departments.dept_no = dept_emp.dept_no;
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B"
+select first_name, last_name, sex
+from employees
+where first_name = 'Hercules' and last_name like 'B%';
 
